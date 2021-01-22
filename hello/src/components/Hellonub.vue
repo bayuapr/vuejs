94cdfea53b5c3f64eb6876760b274a53c705dc59
bayuapr <bayuapriansyah7@gmail.com>
2021-01-22 10:09:16 +0700

    belajar style

--- hello/src/components/Hellonub.vue
+++ hello/src/components/Hellonub.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="hellonub-wrapper">
-    <h2 class="title-header">Ini Logo</h2>
+    <h2 class="title-header">Support by:</h2>
     <div class="item-hellonub">
-    <img class="logo" alt="Vue logo" src="../assets/nodejs-seeklogo.com.svg">
+    <img class="logo" alt="Node.js" src="../assets/nodejs-seeklogo.com.svg">
     </div>
     <div class="item-hellonub">
-    <img class="logo" alt="Vue logo" src="../assets/visual-studio-code-seeklogo.com.svg">
+    <img class="logo" alt="vscode" src="../assets/visual-studio-code-seeklogo.com.svg">
     </div>
     <div class="item-hellonub">
-    <img class="logo" alt="Vue logo" src="../assets/npm-seeklogo.com.svg">
+    <img class="logo" alt="npm" src="../assets/npm-seeklogo.com.svg">
     </div>
     <div class="item-hellonub">
-    <img class="logo" alt="Vue logo" src="../assets/git-seeklogo.com.svg">
+    <img class="logo" alt="git" src="../assets/git-seeklogo.com.svg">
     </div>
   </div>
 </template>
@@ -23,6 +23,10 @@ export default {
 </script>
 
 <style scoped>
+.title-header {
+  margin-bottom: 80px;
+  color: aliceblue;
+}
 .hellonub-wrapper {
   width: 70%;
   overflow: hidden;
