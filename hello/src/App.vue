3dd8e65b449bca50f9686b761a0915f423f4f9eb
bayuapr <bayuapriansyah7@gmail.com>
2021-01-22 09:44:46 +0700

    vue cli basic

---
+++ hello/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Hellonub />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Hellonub from './components/Hellonub.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hellonub
+  }
+}
+</script>
+
+<style>
+body {
+ background: rgb(2, 1, 61);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello/src/components/Hellonub.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hellonub-wrapper">
+    <h2 class="title-header">Ini Logo</h2>
+    <div class="item-hellonub">
+    <img class="logo" alt="Vue logo" src="../assets/nodejs-seeklogo.com.svg">
+    </div>
+    <div class="item-hellonub">
+    <img class="logo" alt="Vue logo" src="../assets/visual-studio-code-seeklogo.com.svg">
+    </div>
+    <div class="item-hellonub">
+    <img class="logo" alt="Vue logo" src="../assets/npm-seeklogo.com.svg">
+    </div>
+    <div class="item-hellonub">
+    <img class="logo" alt="Vue logo" src="../assets/git-seeklogo.com.svg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hellonub",
+};
+</script>
+
+<style scoped>
+.hellonub-wrapper {
+  width: 70%;
+  overflow: hidden;
+  margin: auto;
+  /* background: rgba(26, 25, 25, 0.781); */
+}
+
+.item-hellonub {
+  float: left;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  width: 150px;
+  height: 150px;
+}
+
+.item-hellonub .logo {
+  
+  width: 150px;
+  height: 150px;
+}
+</style>
