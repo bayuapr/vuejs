63c7c8a723a708fa4c611c6596cfe89fb74c79fa
bayuapr <bayuapriansyah7@gmail.com>
2021-01-25 07:30:17 +0700

    belajar route & bootstrap

---
+++ govue/src/views/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="media">
+            <img class="mr-3" />
+            <div class="media-body">
+              <h5 class="mt-0">Media heading</h5>
+              <p>
+                Will you do the same for me? It's time to face the music I'm no
+                longer your muse. Heard it's beautiful, be the judge and my
+                girls gonna take a vote. I can feel a phoenix inside of me.
+                Heaven is jealous of our love, angels are crying from up above.
+                Yeah, you take me to utopia.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "Profile",
+  components: {},
+};
+</script>
